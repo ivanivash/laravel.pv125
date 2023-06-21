505c784d5f6b443bbbba2ca0c9c02fd9fa114837
Іван Івашинюта <ivanivash@ukr.net>
2023-06-21 20:11:21 +0300

    Gitignore issue fixed

---
+++ local.pv125.com/head.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP_ivanivash</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<?php include $_SERVER["DOCUMENT_ROOT"]."/header.php"; ?>
